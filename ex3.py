8ef67b3ca45854b4c7148db6b8a8da3acd66e84f
jql <johanna.lichtman@gmail.com>
2014-04-25 13:04:43 -0700

    add ex3.py

---
+++ ex3.py
@@ -0,0 +1,32 @@
+# print text
+print "I will now count my chickens"
+
+
+# print text and data on the same line
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# print text
+print "Now I will count the eggs:"
+
+# print a calculation
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# print text
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# print the result on a new line
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# print text and the result on the same line
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+#repeat
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
